7b535bab97e539d0fe7b27fd766032a461e914d2
kenjimaeda54 <kenjimaedafamily@gmail.com>
2021-04-25 00:22:49 -0300

    tipando variaveis

---
+++ src/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    semi: true,
+
+    trailingComma: "all",
+
+    singleQuote: true,
+
+    printWidth: 80,
+
+    tabWidth: 2,
+
+};
---
+++ src/aula1/types.ts
@@ -0,0 +1,22 @@
+//numeros
+let nome: string = "Beatriz";
+let numbers: number = 1;
+let adult: boolean = true;
+
+//array
+const peoples: Array<string> = ["Beatriz","Ricardo","Erika"];
+const people: string[] = ["Beatriz","Ricardo","Rafael"];
+
+//objetos
+let data: {nome: string,idade: number,adult?:boolean}={
+  nome:"Ricardo",
+  idade:20,
+};
+
+//funções
+function soma(x:number,y:number){
+    return x+y;
+}
+
+const plural: (p:number,z:number) => number = (x,y) =>  x+y;
+
