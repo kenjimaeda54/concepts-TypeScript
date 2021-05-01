fa2b023ff3024b3b35e6320d923375cf40df5f51
kenjimaeda54 <kenjimaedafamily@gmail.com>
2021-05-01 20:11:08 -0300

    assoiação

--- src/POO/association/association.ts
+++ src/POO/association/association.ts
@@ -1,14 +1,14 @@
 //esse tipo de assoiação e ligação fraca,porque estamos apenas
 //usando as classes  para determinada coisa sem ambas
 //necessariamente depender uma das outras
-export class Escrictor {
-  protected _ferrament: Ferrament | null = null;
+export class Escritor {
+  private _ferrament: Ferrament | null = null;
   constructor(private _name: string) {}
 
   get name(): string {
     return this._name;
   }
-  //utilizando metodo set para associar a classe ferramenta
+
   set ferrament(ferrament: Ferrament | null) {
     this._ferrament = ferrament;
   }
@@ -16,6 +16,7 @@ export class Escrictor {
   get ferrament(): Ferrament | null {
     return this._ferrament;
   }
+
   write(): void {
     if (this.ferrament === null) {
       console.log('Eu não consigo escrever');
@@ -26,35 +27,40 @@ export class Escrictor {
 }
 
 export abstract class Ferrament {
-  constructor(private _name: string) {}
-  abstract write(): void;
+  constructor(protected _name: string) {}
 
   get name(): string {
     return this._name;
   }
+  abstract write(): void;
 }
 
 export class Pen extends Ferrament {
   write(): void {
-    console.log(`${this.name} esta escrevendo `);
+    return console.log(`${this.name} esta escrevendo`);
   }
 }
 
 export class Machine extends Ferrament {
   write(): void {
-    console.log(`${this.name} esta digitando`);
+    return console.log(`${this.name} esta digitando`);
   }
 }
 
-const write1 = new Pen('Bic');
-const write2 = new Machine('Computer');
-const write3 = new Escrictor('Robot');
+const pens = new Pen('Bic');
+const machines = new Machine('Computer');
+const escritors = new Escritor('Luiz');
 
-console.log(write1.name);
-console.log(write2.name);
-console.log(write3.name);
+console.log(pens.name);
+console.log(machines.name);
+console.log(escritors.name);
 
-//por ser uma assoiação é possivel utilizar o metodo ferrament apenas
-//com unica vez com metodo write
-write3.ferrament = write1;
-write3.write();
+//por ser uma assoiação é possivel utilizar o metodo ferrament uma
+// unica vez com metodo write
+//aqui é o exemplo de associação, com metodo setter e getter
+//eu faço associação
+
+escritors.ferrament = pens;
+escritors.ferrament = machines;
+
+escritors.write();
