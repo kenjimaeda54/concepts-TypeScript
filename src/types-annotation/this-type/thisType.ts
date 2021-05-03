83a1774a984aea719eab6d4dd56c14144dfc85ac
kenjimaeda54 <kenjimaedafamily@gmail.com>
2021-05-03 19:25:44 -0300

    tipo this

--- src/types-annotation/this-type/thisType.ts
+++ src/types-annotation/this-type/thisType.ts
@@ -26,6 +26,35 @@ export class subCalc extends Calc {
   }
 }
 
-const cal = new Calc(10);
-cal.mult(3).divison(2).subtraction(5);
+// com retorno this consigo realizar chamada da classe em cadeia
+const cal = new Calc(33);
+cal.mult(3).divison(2).subtraction(5).sum(5);
 console.log(cal);
+
+//projeto padrãa GOF
+
+export class requests {
+  protected method: 'get' | 'post' | null = null;
+  protected url: string | null = null;
+
+  //vale observar,não posso colocar | sring,porque string e mais amplo
+  // 'get' e 'post'
+  setMethod(item: 'get' | 'post'): this {
+    this.method = item;
+    return this;
+  }
+
+  //vale observar que aqui posso colocar 'get' | 'post',porque e mais
+  //restrito que string
+  getUrl(item: string): this {
+    this.url = item;
+    return this;
+  }
+
+  send(): void {
+    console.log(`Enviando atraves do metodo ${this.method} a url:${this.url}`);
+  }
+}
+
+const request = new requests();
+request.setMethod('get').getUrl('http://gooogle.com').send();
